6e385e6a24b98dd34fcd963576401f2a81ace93c
Ryan Hyma <Ryan.Hyma@gmail.com>
2025-08-14 07:40:30 -0400

    fix: standardize formatting in Route53 record resource for clarity

--- terragrunt/aws/load_balancer/certificate.tf
+++ terragrunt/aws/load_balancer/certificate.tf
@@ -16,10 +16,10 @@ resource "aws_acm_certificate" "ai_answers" {
 resource "aws_route53_record" "ai_answers_certificate_validation" {
   for_each = {
     for dvo in aws_acm_certificate.ai_answers.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-  zone_id = var.hosted_zone_id
+      name    = dvo.resource_record_name
+      type    = dvo.resource_record_type
+      record  = dvo.resource_record_value
+      zone_id = var.hosted_zone_id
     }
   }
 
